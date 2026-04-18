4a11fa4a39a2ce64043a67fb3e6589a14be9b43f
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2026-04-18 09:19:54 +0200

    Add initial Share Extension with UI and basic functionality
    - Create ShareViewController subclass of SLComposeServiceViewController with stub methods
    - Add MainInterface.storyboard defining ShareViewController UI layout
    - Add Info.plist configuring share extension properties and activation rule
    - Add empty ShareExtension.entitlements file for extension permissions

---
+++ ShareExtension/ShareViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by Arturo Carretero Calvo on 18/04/2026.
+//  Copyright © 2026 Arturo Carretero Calvo. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
